c0d54c268d0c5d62cc4168d2099e02f5abd5117a
BestOfQ <3434614710@qq.com>
2020-01-23 19:54:56 +0800

    用户请求处理

--- todolist/views.py
+++ todolist/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render(request, 'todolist/home.html')
+	# request.POST.get('待办事项')
+	context = {'待办事项': request.POST.get('待办事项')}
+	return render(request, 'todolist/home.html', context)
 
 def about(request):
 	return render(request, 'todolist/about.html')
