06a132a4b41bec7db4bb5c7093679dd3f5df6765
BestOfQ <3434614710@qq.com>
2020-01-24 20:48:18 +0800

    列表——增

--- todolist/views.py
+++ todolist/views.py
@@ -2,10 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 没有数据库之前 使用列表储存数据
+lst = [
+    {'待办事项': '遛狗', '已完成': False},
+    {'待办事项': '武汉加油', '已完成': True},
+]
+
 def home(request):
-	# request.POST.get('待办事项')
-	context = {'待办事项': request.POST.get('待办事项')}
-	return render(request, 'todolist/home.html', context)
+    #声明全局变量
+    global lst
+    # 判断是get 还是post
+    if request.method == 'POST':
+        # request.POST.get('待办事项')
+        if request.POST.get('待办事项') == '':
+            return render(request, 'todolist/home.html', {'警告':'请输入内容'})
+        else:
+            text = request.POST.get('待办事项')
+            lst.append({'待办事项': text, '已完成': False})
+            # context = {'待办事项': request.POST.get('待办事项')}
+            context = {'清单': lst}
+            return render(request, 'todolist/home.html', context)
+    elif request.method == 'GET':
+        context = {'清单': lst}
+        return render(request, 'todolist/home.html', context)
 
 def about(request):
 	return render(request, 'todolist/about.html')
