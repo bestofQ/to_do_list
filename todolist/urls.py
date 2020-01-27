f7e116cb47b5357adef74fec9cfaa1e68ebaa340
BestOfQ <3434614710@qq.com>
2020-01-27 13:26:28 +0800

    新增——删除url

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('home/', views.home, name='todolist_home'),
 	path('about/', views.about, name='todolist_about'),
 	path('edit/', views.edit, name='todolist_edit'),
+    path('delete/<int:forloop_counter>', views.delete, name='todolist_delete'),
 ]
