d77d38fc6747598f8c799b6445aae31ae41e90ee
BestOfQ <3434614710@qq.com>
2020-01-28 14:30:52 +0800

    新增——修改

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'todolist'
 urlpatterns = [
-	path('home/', views.home, name='todolist_home'),
-	path('about/', views.about, name='todolist_about'),
-	path('edit/', views.edit, name='todolist_edit'),
+    path('home/', views.home, name='todolist_home'),
+    path('about/', views.about, name='todolist_about'),
+    # path('edit/', views.edit, name='todolist_edit'),
+    path('edit/<int:forloop_counter>', views.edit, name='todolist_edit'),
     path('delete/<int:forloop_counter>', views.delete, name='todolist_delete'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -27,12 +27,22 @@ def home(request):
         return render(request, 'todolist/home.html', context)
 
 def delete(request, forloop_counter):
+    global lst
     lst.pop(forloop_counter-1)
     # redirect('网址')
     return redirect("todolist:todolist_home")
 
 def about(request):
-	return render(request, 'todolist/about.html')
+    return render(request, 'todolist:todolist/about.html')
 
-def edit(request):
-	return render(request, 'todolist/edit.html')
+def edit(request, forloop_counter):
+    global lst
+    if request.method == 'GET':
+        context = {'待修改事项': lst[int(forloop_counter) - 1]['待办事项']}
+        return render(request, "todolist/edit.html", context)
+    elif request.method == 'POST':
+        if request.POST.get('待修改事项') == '':
+            return render(request, 'todolist/edit.html', {'警告':'请输入内容'})
+        else:
+            lst[int(forloop_counter) - 1]['待办事项'] = request.POST.get('待修改事项')
+            return redirect("todolist:todolist_home")
