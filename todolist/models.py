9253dac5cb4b1d2c29c835bd8ac1ca548b9b9f47
BestOfQ <3434614710@qq.com>
2020-02-05 00:41:19 +0800

    创建model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+
+    thing = models.CharField(max_length=50)
+    done = models.BooleanField(default=False)
+
+    # class Meta:
+    #     verbose_name = "Todo"
+    #     verbose_name_plural = "Todos"
+
+    def __str__(self):
+        pass
+    
